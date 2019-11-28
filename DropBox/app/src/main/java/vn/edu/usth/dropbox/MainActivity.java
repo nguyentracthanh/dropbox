1ef2d8d3e8536b062db354aaddc069e96eb29fd3
Thanh <thanhnt.b8170@st.usth.edu.vn>
2019-11-28 09:11:42 +0700

    first fragment of dropbox

---
+++ DropBox/app/src/main/java/vn/edu/usth/dropbox/MainActivity.java
@@ -0,0 +1,19 @@
+package vn.edu.usth.dropbox;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+    }
+}
